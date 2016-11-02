9c5882c9057f829accc67948a16de7c6f364460d
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-11-02 16:24:47 -0400

    Done.

--- public/js/cars.js
+++ public/js/cars.js
@@ -8,15 +8,36 @@ function formatCars(carsJSON) {
   // refer to app/views/index.erb lines 16 - 22 for an example of how
   // to format three cars, each in a div with a class "col-md-4", in a 
   // div with a class "row"
+  let formattedHtml = '<div class="row">'
+  // loop through the carsJSON objects
+  for (let i in carsJSON) {
+    // add the col-md-4 to the formattedHtml
+    formattedHtml += `<div class="col-md-4 car"><h2>${carsJSON[i]["Make"]}</h2><p><strong>Model:</strong> ${carsJSON[i]["Model"]}</p><p><strong>Year:</strong> ${carsJSON[i]["Year"]}</p></div>`
+  }
+  formattedHtml += '</div>'
+
+  return formattedHtml;
 }
 
 function addCarsToDOM(carsJSON) {
   // this function should pass carsJSON to formatCars() and then 
   // add the resulting HTML to the div with an id of "cars"
+  let formattedCars = formatCars(carsJSON);
+  $('#cars').append(formattedCars);
 }
 
 function fetchJSON() {
   // this function will make the ajax call
   // on success of the ajax call, it will pass the returned data
   // to addCarsToDOM()
+  let pageNumber = $('#cars .row').length + 1;
+  $.ajax({
+        url: `${baseUrl}/${pageNumber}/3`,
+        contentType: 'application/json',
+        dataType: 'jsonp',
+        success: function(data) {
+          addCarsToDOM(data);
+        }
+      });
+
 }
